5b6eda55abb01000854b2205ba643ae43bf8c4f1
Nick <njhuemmer@gmail.com>
2022-06-15 19:27:59 -0400

    add comment capability

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,53 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import { getPosts } from 'lib/data.js'
+import prisma from 'lib/prisma'
+import Posts from 'components/Posts'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
 
+export default function Home({ posts }) {
+  const { data: session, status } = useSession()
+  const router = useRouter()
+  const loading = status === 'loading'
 
-export default function Home() {
+  if (loading) {
+    return null
+  }
+
+  if (session && !session.user.name) {
+    router.push('/setup')
+  }
+
   return (
-    <div >
-      <p>Welcome!</p>
+    <div>
+      <Head>
+        <title>Reddit Clone</title>
+        <meta name='description' content='A great social network!' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <header className='bg-black text-white h-12 flex pt-3 px-5 pb-2'>
+        <p>Reddit clone</p>
+        <p className='grow'></p>
+        <a
+          className='flex-l border px-4 font-bold rounded-full mb-1'
+          href={session ? '/api/auth/signout' : '/api/auth/signin'}
+        >
+          {session ? 'logout' : 'login'}
+        </a>
+      </header>
+
+      <Posts posts={posts} />
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  let posts = await getPosts(prisma)
+  posts = JSON.parse(JSON.stringify(posts))
+
+  return {
+    props: {
+      posts: posts,
+    },
+  }
 }
---
+++ pages/r/[subreddit].js
@@ -0,0 +1,40 @@
+import Link from 'next/link';
+import prisma from 'lib/prisma'
+import { getSubreddit, getPostsFromSubreddit } from 'lib/data.js'
+
+import Posts from 'components/Posts'
+
+export default function Subreddit({ subreddit, posts }) {
+    if (!subreddit) {
+        return <p className='text-center p-5'>Subreddit does not exist 😞</p>
+    }
+
+    return (
+        <>
+            <header className='bg-black text-white h-12 flex pt-3 px-5 pb-2'>
+                <Link href={`/`}>
+                    <a className='underline'>Home</a>
+                </Link>
+                <p className='grow'></p>
+            </header>
+            <header className='bg-black text-white h-12 flex pt-3 px-5 pb-2'>
+                <p className='text-center'>/r/{subreddit.name}</p>
+                <p className='ml-4 text-left grow'>{subreddit.description}</p>
+            </header>
+            <Posts posts={posts} />
+        </>
+    )
+}
+
+export async function getServerSideProps({ params }) {
+    const subreddit = await getSubreddit(params.subreddit, prisma)
+    let posts = await getPostsFromSubreddit(params.subreddit, prisma)
+    posts = JSON.parse(JSON.stringify(posts))
+
+    return {
+        props: {
+            subreddit,
+            posts,
+        },
+    }
+}
